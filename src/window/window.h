d7c70000e488bbe064af33e6d3f84105d4033f1e
IzaltinoDomingosDeSouza <izaltinodsouza@gmail.com>
2026-07-08 22:16:19 -0300

    add more thing to window class

--- src/window/window.h
+++ src/window/window.h
@@ -6,8 +6,20 @@
 class Window
 {
 public:
-    virtual void init() = 0;
+    Window() = default;
+    virtual ~Window() = default;
+
+    Window(const Window &) = delete;
+    Window & operator=(const Window &) = delete;
+    Window(Window &&) = delete;
+    Window & operator=(Window &&) = delete;
+
+    virtual bool init(const std::string & title, size_t width, size_t height) = 0;
     virtual void shutdown() = 0;
+
+    virtual bool should_close() = 0;
+    virtual void poll_events() = 0;
+    virtual void swap_buffers() = 0;
 
     virtual void set_title(const std::string & title) = 0;
     virtual const std::string & get_title() const = 0;
@@ -16,4 +28,9 @@ public:
     virtual size_t get_width() const = 0;
     virtual void set_height(size_t height) = 0;
     virtual size_t get_height() const = 0;
+
+    virtual void set_vsync(bool enabled) = 0;
+    virtual bool is_vsync() const = 0;
+
+    virtual void apply_changes() = 0;
 };
